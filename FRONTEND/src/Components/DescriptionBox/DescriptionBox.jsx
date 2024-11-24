6516c1e9b82ad77efa89c97b6bda5ac69cda6b8b
AHMAD <nawaz311412@gmail.com>
2024-11-24 19:37:52 +0500

    complete the front-end of website

---
+++ FRONTEND/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./DescriptionBox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+          molestias natus accusamus reprehenderit laudantium provident
+          recusandae alias aut ad quidem dolorum a deleniti doloremque eos
+          voluptatum fuga cumque corrupti optio obcaecati iusto odit amet, unde
+          omnis velit. Architecto laborum ullam eum rem minus obcaecati
+          praesentium maiores aut rerum consequuntur. Nesciunt rem soluta atque
+          iste molestiae ipsam in, qui voluptatum illum, natus eligendi
+          praesentium sint voluptatibus inventore mollitia? Excepturi rem ipsam
+          voluptate nam cum veniam voluptatibus corporis nisi numquam recusandae
+          beatae perferendis, laborum sunt architecto, in optio quae minus.
+          Velit soluta sit tenetur minus voluptas vel optio rerum eaque
+          accusamus error.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse
+          sequi explicabo hic sint. Modi quia ratione assumenda optio odio
+          perspiciatis alias quasi, possimus perferendis voluptatum architecto
+          impedit illo nisi obcaecati laboriosam?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
